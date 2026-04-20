ae438a4edc71c5a59d94422ccbde371a6e6d967e
Pythonjonas <Kajonas38@gmail.com>
2026-04-20 12:22:41 +0100

    Adding commas to test

--- Source/MGP_2526/MyActorComponent.cpp
+++ Source/MGP_2526/MyActorComponent.cpp
@@ -1,40 +1,40 @@
-#include "DetectionComponent.h"
+//#include "DetectionComponent.h"
 
 // Sets default values
-UDetectionComponent::UDetectionComponent()
-{
-    PrimaryComponentTick.bCanEverTick = true;
+//UDetectionComponent::UDetectionComponent()
+//{
+    //PrimaryComponentTick.bCanEverTick = true;
 
-    DetectionLevel = 0.f;
-    MaxDetection = 100.f;
-    DetectionRate = 20.f;
-    DetectionDecayRate = 5.f;
-}
+   // DetectionLevel = 0.f;
+   // MaxDetection = 100.f;
+  //  DetectionRate = 20.f;
+ //   DetectionDecayRate = 5.f;
+//}
 
-void UDetectionComponent::BeginPlay()
-{
-    Super::BeginPlay();
-}
+//void UDetectionComponent::BeginPlay()
+//{
+//    Super::BeginPlay();
+//}
 
-void UDetectionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+//void UDetectionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+//{
+    //Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
     // Automatically decay detection over time
-    ReduceDetection(DeltaTime);
-}
+  //  ReduceDetection(DeltaTime);
+//}
 
-void UDetectionComponent::AddDetection(float Amount)
-{
-    DetectionLevel = FMath::Clamp(DetectionLevel + Amount, 0.f, MaxDetection);
-}
+//void UDetectionComponent::AddDetection(float Amount)
+//{
+ //   DetectionLevel = FMath::Clamp(DetectionLevel + Amount, 0.f, MaxDetection);
+//}
 
-void UDetectionComponent::ReduceDetection(float DeltaTime)
-{
-    DetectionLevel = FMath::Clamp(DetectionLevel - DetectionDecayRate * DeltaTime, 0.f, MaxDetection);
-}
+//void UDetectionComponent::ReduceDetection(float DeltaTime)
+//{
+ //   DetectionLevel = FMath::Clamp(DetectionLevel - DetectionDecayRate * DeltaTime, 0.f, MaxDetection);
+//}
 
-float UDetectionComponent::GetDetectionPercent() const
-{
-    return DetectionLevel / MaxDetection;
-}
+//float UDetectionComponent::GetDetectionPercent() const
+//{
+  //  return DetectionLevel / MaxDetection;
+//}
--- Source/MGP_2526/MyActorComponent.h
+++ Source/MGP_2526/MyActorComponent.h
@@ -1,49 +1,49 @@
-#pragma once
+//#pragma once
 
-#include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
-#include "DetectionComponent.generated.h"
+//#include "CoreMinimal.h"
+//#include "Components/ActorComponent.h"
+//#include "DetectionComponent.generated.h"
 
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
-class YOURGAME_API UDetectionComponent : public UActorComponent
-{
-    GENERATED_BODY()
+//UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+//class YOURGAME_API UDetectionComponent : public UActorComponent
+//{
+  //  GENERATED_BODY()
 
-public:
+//public:
     // Sets default values for this component's properties
-    UDetectionComponent();
+    //UDetectionComponent();
 
-protected:
+//protected:
     // Called when the game starts
-    virtual void BeginPlay() override;
-
-public:
+  //virtual void BeginPlay() override;
+  
+//public:
     // Called every frame
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+  //  virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
     // Current detection level
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
-    float DetectionLevel;
+  //  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
+   // float DetectionLevel;
 
     // Maximum detection
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
-    float MaxDetection;
+   // UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
+    //float MaxDetection;
 
     // How fast detection increases
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
-    float DetectionRate;
+  //  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
+   // float DetectionRate;
 
     // How fast detection decreases
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
-    float DetectionDecayRate;
+   // UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Detection")
+  //  float DetectionDecayRate;
 
     // Increase detection
-    void AddDetection(float Amount);
+    //void AddDetection(float Amount);
 
     // Reduce detection over time
-    void ReduceDetection(float DeltaTime);
+    //void ReduceDetection(float DeltaTime);
 
     // Returns a 0-1 value for UI
-    UFUNCTION(BlueprintCallable, Category = "Detection")
-    float GetDetectionPercent() const;
-};
+    //UFUNCTION(BlueprintCallable, Category = "Detection")
+    //float GetDetectionPercent() const;
+//};
