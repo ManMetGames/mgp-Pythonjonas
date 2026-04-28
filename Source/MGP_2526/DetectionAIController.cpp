0193fbaccc5682df85473e07ddb40f604135d380
Pythonjonas <Kajonas38@gmail.com>
2026-04-28 16:24:02 +0100

    fixing more locomotion

--- Source/MGP_2526/DetectionAIController.cpp
+++ Source/MGP_2526/DetectionAIController.cpp
@@ -1,17 +1,14 @@
 #include "DetectionAIController.h"
-#include "EnemyAnimInstance.h"
-#include "GameFramework/Character.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/BehaviorTree.h"
+#include "GameFramework/Character.h"
 
 ADetectionAIController::ADetectionAIController()
 {
     PerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("PerceptionComp"));
-    SetPerceptionComponent(*PerceptionComp);
 
     HearingConfig = CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("HearingConfig"));
     HearingConfig->HearingRange = 1000.f;
-    HearingConfig->SetMaxAge(10.f);
     HearingConfig->DetectionByAffiliation.bDetectEnemies = true;
     HearingConfig->DetectionByAffiliation.bDetectNeutrals = true;
     HearingConfig->DetectionByAffiliation.bDetectFriendlies = true;
@@ -23,88 +20,75 @@ ADetectionAIController::ADetectionAIController()
 void ADetectionAIController::BeginPlay()
 {
     Super::BeginPlay();
-  
 
-    // Initialize blackboard
-    UBlackboardComponent* BB = GetBlackboardComponent();
-    if (BB)
-    {
-        BB->SetValueAsBool(FName("IsSuspicious"), false);
-        BB->SetValueAsBool(FName("IsSearching"), false);
-        BB->SetValueAsBool(FName("IsAlerted"), false);
-    }
     if (BehaviorTree)
     {
         RunBehaviorTree(BehaviorTree);
-        UE_LOG(LogTemp, Warning, TEXT("Behavior Tree started!"));
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("No Behavior Tree assigned!"));
     }
 
-
-    PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(
-        this,
-        &ADetectionAIController::OnPerceptionUpdated
-    );
+    PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &ADetectionAIController::OnPerceptionUpdated);
 }
-void ADetectionAIController::SetDetectionState(EDetectionState NewState)
-{
-    CurrentState = NewState;
 
-    UBlackboardComponent* BB = GetBlackboardComponent();
-    if (!BB)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("No Blackboard!"));
-        return;
-    }
-    
-
-    BB->SetValueAsBool(FName("IsSuspicious"), NewState == EDetectionState::Suspicious);
-    BB->SetValueAsBool(FName("IsSearching"), NewState == EDetectionState::Searching);
-    BB->SetValueAsBool(FName("IsAlerted"), NewState == EDetectionState::Alert);
-
-    UE_LOG(LogTemp, Warning, TEXT("State set via Blackboard!"));
-}
 void ADetectionAIController::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-    UE_LOG(LogTemp, Warning, TEXT("Perception updated!"));
-
     if (Stimulus.WasSuccessfullySensed())
     {
         LastHeardLocation = Stimulus.StimulusLocation;
-        bIsInvestigating = true;
-        MoveToLocation(LastHeardLocation, 50.f);
+        float Distance = FVector::Dist(GetPawn()->GetActorLocation(), LastHeardLocation);
 
-        if (CurrentState == EDetectionState::Idle)
-            SetDetectionState(EDetectionState::Suspicious);
-        else if (CurrentState == EDetectionState::Suspicious)
-            SetDetectionState(EDetectionState::Searching);
-        else if (CurrentState == EDetectionState::Searching)
-            SetDetectionState(EDetectionState::Alert);
-
-        GetWorldTimerManager().SetTimer(
-            ResetTimerHandle,
-            this,
-            &ADetectionAIController::ResetToIdle,
-            10.f,
-            false
-		);
-        
-
-            UBlackboardComponent* BB = GetBlackboardComponent();
+        // Update Blackboard immediately
+        UBlackboardComponent* BB = GetBlackboardComponent();
         if (BB)
         {
             BB->SetValueAsVector(FName("LastHeardLocation"), LastHeardLocation);
+            BB->SetValueAsFloat(FName("DetectionDistance"), Distance);
         }
- 
+
+        // --- State Transition Logic ---
+        if (Distance < 300.f)
+        {
+            // Close sound: Direct search
+            SetDetectionState(EDetectionState::Searching);
+        }
+        else
+        {
+            // Far sound: Suspicious first, then Search if heard again
+            if (CurrentState == EDetectionState::Idle)
+            {
+                SetDetectionState(EDetectionState::Suspicious);
+            }
+            else if (CurrentState == EDetectionState::Suspicious)
+            {
+                SetDetectionState(EDetectionState::Searching);
+            }
+        }
+
+        // Restart the "Give Up" timer
+        GetWorldTimerManager().ClearTimer(ResetTimerHandle);
+        GetWorldTimerManager().SetTimer(ResetTimerHandle, this, &ADetectionAIController::ResetToIdle, 10.f, false);
+
+        // Move to the noise
+        MoveToLocation(LastHeardLocation, 50.f);
+    }
+}
+
+void ADetectionAIController::SetDetectionState(EDetectionState NewState)
+{
+    CurrentState = NewState;
+    UBlackboardComponent* BB = GetBlackboardComponent();
+    if (BB)
+    {
+        BB->SetValueAsBool(FName("IsSuspicious"), CurrentState == EDetectionState::Suspicious);
+        BB->SetValueAsBool(FName("IsSearching"), CurrentState == EDetectionState::Searching);
+        BB->SetValueAsBool(FName("IsAlerted"), CurrentState == EDetectionState::Alert);
     }
 }
 
 void ADetectionAIController::ResetToIdle()
 {
-    bIsInvestigating = false;
     SetDetectionState(EDetectionState::Idle);
-    UE_LOG(LogTemp, Warning, TEXT("Resetting to Idle."));
+
+    // Optional: Return to start position logic would go here
+    StopMovement();
+    UE_LOG(LogTemp, Warning, TEXT("AI gave up and returned to Idle."));
 }
--- Source/MGP_2526/DetectionAIController.h
+++ Source/MGP_2526/DetectionAIController.h
@@ -2,9 +2,9 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Hearing.h"
-#include "BehaviorTree/BehaviorTree.h"
 #include "DetectionAIController.generated.h"
 
 UENUM(BlueprintType)
@@ -16,30 +16,28 @@ enum class EDetectionState : uint8
     Alert
 };
 
-
 UCLASS()
 class MGP_2526_API ADetectionAIController : public AAIController
 {
     GENERATED_BODY()
-    UPROPERTY(EditAnywhere, Category = "AI")
-    UBehaviorTree* BehaviorTree;
-
-
 
 public:
     ADetectionAIController();
 
-    UPROPERTY(BlueprintReadOnly, Category = "Detection", meta = (AllowPrivateAccess = "true"))
+    UPROPERTY(BlueprintReadOnly, Category = "Detection")
     EDetectionState CurrentState = EDetectionState::Idle;
-    UPROPERTY(BlueprintReadOnly, Category = "Detection", meta = (AllowPrivateAccess = "true"))
+
+    UPROPERTY(BlueprintReadOnly, Category = "Last Heard Location")
     FVector LastHeardLocation;
+
+    UPROPERTY(EditAnywhere, Category = "AI")
+    UBehaviorTree* BehaviorTree;
 
 protected:
     virtual void BeginPlay() override;
 
     UFUNCTION()
     void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
-
     void SetDetectionState(EDetectionState NewState);
     void ResetToIdle();
     
